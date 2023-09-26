302847cd6b7e3e3ab0049e96eda6d7b7c95ceea8
Edoardo Polce <edoardo.polce@euris.it>
2023-09-26 12:04:12 +0200

    [FE] Change view products

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,5 +1,57 @@
+import React, {useMemo, useState} from "react";
+import API from "../api";
+import Spinner from "./Spinner"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars, faTableCells} from "@fortawesome/free-solid-svg-icons";
+
+
 export function Products() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [toggleView, setToggleView] = useState('row');
+
+    const getProducts = () => {
+        API.get(`stores/ijpxNJLM732vm8AeajMR/products`).then(
+            res => {
+                setProducts(res.data)
+                setLoading(false);
+            }
+        );
+
+    }
+    useMemo(() => {
+        getProducts()
+    }, []);
+
+    function renderProduct(p) {
+        return (
+            <div key={p.id} className="card">
+                <h3>{p.data.name}</h3>
+                <p>${p.data.description}</p>
+                <p>${p.data.price}</p>
+                <p>${p.data.employee}</p>
+            </div>
+        );
+    }
+
+    if (loading) return <Spinner/>;
+
     return (
-            <div>TEST</div>
-    )
+        <>
+            <div className="container">
+                <div className="change-view">
+                    <div className={toggleView === 'row' ? 'active' : 'inactive'}>
+                        <FontAwesomeIcon size={"lg"} onClick={() => setToggleView('row')}
+                                         className={'icon'} icon={faBars}/>
+                    </div>
+                    <div className={toggleView === 'columns' ? 'active' : 'inactive'}>
+                        <FontAwesomeIcon size={"lg"} onClick={() => setToggleView('columns')}
+                                         className={'icon'}
+                                         icon={faTableCells}/>
+                    </div>
+                </div>
+                <section className={'products ' + toggleView}>{products.map(renderProduct)}</section>
+            </div>
+        </>
+    );
 }
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Spinner() {
+  return (
+    <div className="lds-container">
+      <div className="spinner-border"></div>
+    </div>
+  );
+}
